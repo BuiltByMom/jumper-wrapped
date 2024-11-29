c2d4da26d0d54ad9e30ed3fa1cc027b47d0bd950
Majorfi <major@ycorpo.com>
2024-11-29 15:35:41 +0100

    feat: favicons/og

---
+++ pages/[version]/index.tsx
@@ -0,0 +1,40 @@
+import Head from 'next/head';
+
+import type {ReactElement} from 'react';
+
+export default function Home({version}: {version: string}): ReactElement {
+	const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL ? 'https://jumper-wrapped.vercel.app' : 'http://localhost:3000';
+	const ogImageUrl = version ? `${baseUrl}/api/og?version=${version}` : `${baseUrl}/og/og.jpg`;
+
+	return (
+		<>
+			<Head>
+				<meta
+					property={'og:image'}
+					content={ogImageUrl}
+				/>
+				<meta
+					property={'og:image:width'}
+					content={'1200'}
+				/>
+				<meta
+					property={'og:image:height'}
+					content={'630'}
+				/>
+				<meta
+					name={'twitter:image'}
+					content={ogImageUrl}
+				/>
+			</Head>
+
+			<div>{`Hello ${version}`}</div>
+		</>
+	);
+}
+
+export async function getServerSideProps(context: any): Promise<{props: {version: string}}> {
+	const version = context.query.version || '';
+	return {
+		props: {version}
+	};
+}
