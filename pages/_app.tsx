c70b55e2afa89782904ad4b0e386bd85432a0ca2
Majorfi <major@ycorpo.com>
2024-11-29 15:20:36 +0100

    feat: favicons/og

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,7 +15,7 @@ export default function App({Component, pageProps}: AppProps): ReactElement {
 				titleColor={'#FFFFFF'}
 				themeColor={'#6120FD'}
 				description={'Your Jumper year in review'}
-				og={'/og/og.jpg'}
+				og={'https://jumper-wrapped.vercel.app/og/og.jpg'}
 				uri={'https://jumper-wrapped.vercel.app'}
 			/>
 		</Providers>
