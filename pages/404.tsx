ad9b543eed0c9460d5209e1111872ca6f72f6520
Majorfi <major@ycorpo.com>
2024-12-06 11:44:59 +0100

    chroes: bg refactor

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,13 +1,13 @@
 import {type ReactElement} from 'react';
 
-import {PageBackgound} from '@/components/Backgrounds';
+import {PageBackground} from '@/components/Backgrounds';
 import {Button404} from '@/components/Button404';
 import {Header} from '@/components/common/Header';
 
 export default function Page404(): ReactElement {
 	return (
 		<div className={'flex h-screen w-full items-center justify-center bg-violet-light'}>
-			<PageBackgound position={'center'} />
+			<PageBackground position={'center'} />
 			<Header
 				set_isWalletSelectorOpen={() => {}}
 				cardsAmount={0}
